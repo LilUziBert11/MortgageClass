16cad1dc10ef799856b840a8553b6e7cb124652b
albert <albert.duenas22@gmail.com>
2022-10-11 18:49:51 -0700

    Final Project

--- src/MortgageCalculatorV3.java
+++ src/MortgageCalculatorV3.java
@@ -1,4 +1,69 @@
-public class MortgageCalculator {
-    private MortgageClass mortgageClass =new MortgageClass();
+/*
+* MortgageCalculatorV3
+*  Programmer: Albert Duenas
+   Date: 10-11-22
+   Version: 1.2
+   Description: A java program that utilizes the mortgage class to calculate mortgage payments. This program prompts the user
+   * for values and passes them in to the mortgage class constructor. Then calls mortgage class methods to calculate and display
+   * mortgage payments.*/
+import java.util.Scanner;
+public class MortgageCalculatorV3 {
+    private static MortgageClass mortgageClass;
 
+    public static String[] userPrompt(){
+        //Declaring new scanner class
+        Scanner sc = new Scanner (System.in);
+
+        //Asking used what principal amount is
+        System.out.println("What is the principal amount?");
+        String principalAmount = sc.nextLine();
+
+        //Asking user what how long the term is
+        System.out.println("What is the term?");
+        String term = sc.nextLine();
+
+        //Asking user what the interest rate is
+        System.out.println("What is the interest rate?");
+        String rate = sc.nextLine();
+
+        String[] userInputs={principalAmount, term, rate};
+        return userInputs;
+    }
+    public static boolean checkForNegative(double value){
+        return value>=0;
+    }
+
+    public static void main(String[] args) {
+        // Grabbing user input and storing it in array
+        String[] userInputs=userPrompt();
+        // Parsing principal amount from string to double
+        double principalAmount=Double.parseDouble(userInputs[0]); // returns double primitive;
+        // Checking principal amount for non-negative
+        if (!checkForNegative(principalAmount)){
+            System.out.println("Please enter a non negative value for principal amount");
+            return;
+        }
+        //Checking principal amount between 100000 and 2000000
+        if (principalAmount<100000 || principalAmount>2000000) {
+            System.out.println("Please enter a value between 100,000 and 1,000,000 (inclusive)");
+            return;
+        }
+        // Parsing term from string to int
+        int term = Integer.parseInt(userInputs[1]);
+        // Checking term for non-negative
+        if (!checkForNegative(term)){
+            System.out.println("Please enter a non negative value for term amount");
+            return;
+        }
+        // Parsing rate from string to double
+        double rate=Double.parseDouble(userInputs[2]);
+        // Checking rate for non-negative
+        if (!checkForNegative(rate)){
+            System.out.println("Please enter a non negative value for the rate amount");
+            return;
+        }
+        mortgageClass = new MortgageClass(principalAmount, term, rate);
+        mortgageClass.calculateMonthlyPayment();
+        mortgageClass.printMonthlyPayment();
+    }
 }
--- src/MortgageClass.java
+++ src/MortgageClass.java
@@ -1,2 +1,63 @@
-package PACKAGE_NAME;public class MortgageClass {
+/*
+* MortgageClass
+*  Programmer: Albert Duenas
+   Date: 10-11-22
+   Version: 1.2
+   Description: A java class that calculates mortgages. Has private data members principal amount, term, and rate.
+   * Has getters and setters for private members. No getters and setters for monthly payment*/
+
+public class MortgageClass {
+    //private data members
+    private double principalAmount;
+    private int term;
+    private double rate;
+    //no getters and setters because this information is controlled
+    private double monthlyPayment =0;
+
+    public MortgageClass(double principalAmount, int term, double rate){
+        this.principalAmount = principalAmount;
+        this.term = term;
+        this.rate = rate;
+
+    }
+    // Getters and Setters
+    public void setPrincipalAmount(double principalAmount){
+        this.principalAmount = principalAmount;
+    }
+
+    public double getPrincipalAmount() {
+        return principalAmount;
+    }
+
+    public void setRate(int term){
+        this.term = term;
+    }
+
+    public double getRate() {
+        return rate;
+    }
+
+    public void setTerm(double rate){
+        this.rate = rate;
+    }
+
+    public int getTerm() {
+        return term;
+    }
+    //class method to calculate monthly payment
+    public void calculateMonthlyPayment(){
+        //Calculating monthly rate
+        rate =  (rate/1200.0);
+
+        //Calculating term in months
+        term = term * 12;
+
+        //Calculating monthly payment
+        monthlyPayment =  (principalAmount * rate / (1.0 - Math.pow(rate + 1, -term)));
+    }
+    //class method to print out monthly payment
+    public void printMonthlyPayment(){
+        System.out.format("This is your monthly payment: %.2f", monthlyPayment);
+    }
 }
+
